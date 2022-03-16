32ee6ae4b9c45442b3d4cef45b37959048ca89bf
ally-petitt <allypetitt@gmail.com>
2022-03-15 21:47:31 -0700

    added exit animations for logos

--- src/components/FrontendSection.js
+++ src/components/FrontendSection.js
@@ -5,6 +5,7 @@ import ScrollTrigger from 'gsap/ScrollTrigger';
 import React, { useEffect, useRef } from 'react'
 import ICONS from "../icons/index.js"
 import "./frontend.css"
+import Projects from './Projects.js';
 
 function FrontendSection() {
   gsap.registerPlugin(ScrollTrigger)
@@ -25,34 +26,48 @@ function FrontendSection() {
 
     timeline.current
      // make text and logos transparent before animations begin
-      .set(textRef.current, { opacity: 0, x: 500 })
+      // .set(textRef.current, { opacity: 0, x: 500 })
       .set(logoRef.current, { opacity: 0, y: 100 })
-      // .set(logosRef.current, { opacity: 0 })
+      .set(logosRef.current, { opacity: 0, y: -100, scale: 0.9 })
 
       // fade text up
-      .from(textRef.current, {
-          y: 900,
-          duration: 2,
-          opacity: 1,
-        })
+      // .from(textRef.current, {
+      //     y: 900,
+      //     duration: 2,
+      //     opacity: 1,
+      //   })
     // move text to left
       .to(textRef.current, .75, {
           x: 0,
           y: 0,
-          duration: 1,
+          duration: 2,
         })
       // fade up react logo
-      .to(logoRef.current,{
+      .to(logoRef.current, 1 ,{
           opacity: 1,
           y: 0,
-          duration: 1
+          duration: 2
         })
       // lower other logos
-      // .from(logosRef.current, {
-      //   y: -100,
-      //   opacity: 1,
-      //   duration: 1
-      // })
+      .to(logosRef.current, {
+        y: 0,
+        opacity: 1,
+        duration: 2,
+        scale: 1
+      })
+      // animate out text
+      .to(textRef.current, 0.8, {
+        rotateX: "90deg",
+        rotateY: "90deg",
+        opacity: "0",
+        x: -400,
+        y: -200,
+        scale: .6,
+        duration: .8,
+
+      })
+      // TODO: make parallax where logos move faster than react logo on scroll
+      .to("#logoContainer", { y: "-100vh", duration: 2, scale: 0.8, opacity: 0})
   
 
   }, [])
@@ -60,9 +75,9 @@ function FrontendSection() {
   return (
     <div>
       <Box bgcolor="primary.main">
-        <div id="wrapper" style={{ position: "sticky", top: "0", height: "3000px", textAlign: "center", justifyContent: "center", display: "flex", maxWidth: "100vw"}}>
-          <div id="textContainer" style={{ width: "50vw", height: "100vh", pointerEvents: "none", position: "sticky", top: "0", textAlign: "center", padding: "40px", display: "flex", alignItems: "center", justifyContent: "center"}}>
-            <Typography color="white" ref={textRef} lineHeight={1.3} variant="h1" m={0} fontWeight="300" letterSpacing={2} component="h3" sx={{ position: "sticky", top: 0 }}>FRONTEND<br/>DEVELOPER</Typography>
+        <div id="wrapper" style={{ position: "sticky", top: "0", textAlign: "center", justifyContent: "center", display: "flex", maxWidth: "100vw"}}>
+          <div id="textContainer" style={{ width: "50vw", height: "200vh", pointerEvents: "none", position: "sticky", top: "0", textAlign: "center", padding: "40px", display: "flex", alignItems: "center", justifyContent: "center"}}>
+            <Typography id="text" color="white" ref={textRef} lineHeight={1.3} variant="h1" m={0} fontWeight="300" letterSpacing={2} component="h3" sx={{ position: "sticky", top: 0 }}>FRONTEND<br/>DEVELOPER</Typography>
           </div>
           <Box id="logoContainer" width="50vw" position="sticky" top={0} display="flex" flexDirection="column" justifyContent="center" alignItems="center" height="100vh">
               <img src={ICONS.react} ref={logoRef} style={{ zIndex: "2", width: "60%", position: "relative", padding: "40px" }}/>
@@ -77,6 +92,7 @@ function FrontendSection() {
               </Box>
           </Box>
         </div>
+        <Projects />
       </Box>
     </div>
   )
---
+++ src/components/Projects.js
@@ -0,0 +1,10 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+
+function Projects() {
+  return (
+    <Typography variant="h2" height="100vh">PROJECTS</Typography>
+  )
+}
+
+export default Projects
